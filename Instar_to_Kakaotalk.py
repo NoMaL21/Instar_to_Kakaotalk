eb2809da930002d5cf748913940c6e4b52d080fe
NoMaL21 <dntjd8509@naver.com>
2024-09-03 21:32:58 +0900

    인스타그램 게시물 사진을 카카오톡 내게 보내기 기능으로 보내주는 기능

---
+++ Instar_to_Kakaotalk.py
@@ -0,0 +1,135 @@
+import requests
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import time
+import os
+
+def load_credentials(file_path):
+    with open(file_path, 'r') as file:
+        credentials = json.load(file)
+    return credentials
+
+def instagram_login(username, password):
+    url = "https://www.instagram.com/accounts/login/"
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
+
+    driver.get(url)
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "username"))
+    )
+
+    username_input = driver.find_element(By.NAME, "username")
+    password_input = driver.find_element(By.NAME, "password")
+    username_input.send_keys(username)
+    password_input.send_keys(password)
+
+    login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
+    login_button.click()
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//a[contains(@href, '/explore/')]"))
+    )
+
+    return driver
+
+def get_latest_instagram_post(driver, target_username):
+    url = f"https://www.instagram.com/{target_username}/"
+    driver.get(url)
+    time.sleep(5)
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    print(soup.prettify())
+
+    images = soup.find_all('img', class_="x5yr21d xu96u03 x10l6tqk x13vifvy x87ps6o xh8yej3")
+    if images:
+        return images[0]['src']
+
+    raise Exception("Failed to find the latest post image URL.")
+
+def refresh_token(app_key, tokens):
+    url = "https://kauth.kakao.com/oauth/token"
+    data = {
+        "grant_type": "refresh_token",
+        "client_id": app_key,
+        "refresh_token": tokens['refresh_token']
+    }
+
+    response = requests.post(url, data=data)
+    result = response.json()
+
+    if 'access_token' in result:
+        tokens['access_token'] = result['access_token']
+    if 'refresh_token' in result:
+        tokens['refresh_token'] = result['refresh_token']
+
+    with open("kakao_code.json", "w") as fp:
+        json.dump(tokens, fp)
+
+    return tokens
+
+# 현재 스크립트의 디렉터리 경로를 얻음
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# chromedriver 경로를 상대경로로 지정
+chromedriver_path = os.path.join(script_dir, 'chromedriver-win32', 'chromedriver-win32', 'chromedriver.exe')
+
+# credentials.json 경로를 상대경로로 지정
+credentials_path = os.path.join(script_dir, 'credentials.json')
+
+# credentials 불러오기
+credentials = load_credentials(credentials_path)
+
+username = credentials["username"]
+password = credentials["password"]
+target_username = credentials["target_username"]
+
+driver = instagram_login(username, password)
+
+try:
+    image_url = get_latest_instagram_post(driver, target_username)
+    print("Latest post image URL:", image_url)
+finally:
+    driver.quit()
+
+with open("kakao_code.json","r") as fp:
+    tokens = json.load(fp)
+
+url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
+headers = {"Authorization" : "Bearer " + tokens["access_token"]}
+
+data = {
+    "template_object": json.dumps({
+        "object_type": "feed",
+        "content": {
+            "title": "오늘의 슈마우스 메뉴",
+            "description": f"https://www.instagram.com/{target_username}/",
+            "image_url": image_url,
+            "image_width": 800,
+            "image_height": 800,
+            "link": {
+                "web_url": image_url,
+                "mobile_web_url": image_url
+            }
+        }
+    })
+}
+
+if image_url:
+    response = requests.post(url, headers=headers, data=data)
+    print(response.status_code)
+    if response.json().get('result_code') == 0:
+        print('메시지를 성공적으로 보냈습니다.')
+    else:
+        print('메시지를 성공적으로 보내지 못했습니다. 오류 메시지 : ' + str(response.json()))
+else:
+    print("이미지 URL을 가져오지 못했습니다.")
